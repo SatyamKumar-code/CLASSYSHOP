1cc9d0aaac4bb03128b517e5f114ab9e2bfc2c38
SatyamKumar-code <satyamkumarsing59@gmail.com>
2025-12-13 17:21:40 +0530

    feat: initialize Admin module with React, Vite, and Tailwind CSS
    - Added package.json with dependencies for React, MUI, Tailwind CSS, and Vite.
    - Included Vite logo SVG in public directory.
    - Created App.css for global styles.
    - Developed main App component with routing setup.
    - Implemented Header component with user account menu and notifications.
    - Built Sidebar component with collapsible submenus for navigation.
    - Added Dashboard page as a placeholder.
    - Included React logo SVG in assets.
    - Configured global CSS imports in index.css.
    - Set up main entry point in main.jsx for rendering the application.
    - Configured Vite with Tailwind CSS plugin in vite.config.js.

---
+++ Admin/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Header from './Components/Header'
+import Sidebar from './Components/Sidebar'
+
+
+function App() {
+  
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      exaxt: true,
+      element: <>
+        <section className='main'>
+          <Header />
+          <div className='contentMain flex'>
+            <div className='sidebarWrapper w-[18%]'>
+              <Sidebar />
+            </div>
+          </div>
+        </section>
+      </>
+    },
+  ])
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
